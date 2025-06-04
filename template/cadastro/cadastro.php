f992d4b11909c989397f545f8da8b73430f4f24d
biaCJ2008 <beatriz_jose@estudante.sesisenai.org.br>
2025-06-04 12:31:17 -0300

    alexandria

---
+++ template/cadastro/cadastro.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="../../static/css/signIn.css">
+    <title>Cadastramento Livro</title>
+    <style>
+       
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Cadastramento Livro</h1>
+        
+        <form>
+            <div class="form-row">
+                <div class="form-group">
+                    <label for="titulo">Título</label>
+                    <input type="text" id="titulo" name="titulo">
+                </div>
+                <div class="form-group">
+                    <label for="autor">Autor</label>
+                    <input type="text" id="autor" name="autor">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group">
+                    <label for="categoria">Categoria</label>
+                    <input type="text" id="categoria" name="categoria">
+                </div>
+                <div class="form-group">
+                    <label for="dataPublicacao">Data Publicação</label>
+                    <input type="date" id="dataPublicacao" name="dataPublicacao">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group">
+                    <label for="isbn">ISBN</label>
+                    <input type="text" id="isbn" name="isbn">
+                </div>
+                <div class="form-group">
+                    <label for="estoque">Estoque</label>
+                    <input type="number" id="estoque" name="estoque">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="sinopse">Sinopse</label>
+                <textarea id="sinopse" name="sinopse" placeholder="Digite a sinopse do livro..."></textarea>
+            </div>
+
+            <div class="button-group">
+                <button type="button" class="btn btn-cancel">Cancelar</button>
+                <button type="submit" class="btn btn-save">Salvar</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
