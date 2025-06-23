20ba82ce05b9682cc92740ec8910d7f9e52add9c
Natalí Alberton <139291235+natalialberton@users.noreply.github.com>
2025-06-23 05:38:09 -0300

    CRUD Membros e Fornecedores

--- template/gestao/fornecedor-gestao.php
+++ template/gestao/fornecedor-gestao.php
@@ -1,11 +1,22 @@
 <?php
 
-require_once "../../funcoes.php";
+session_start();
+require_once '../../geral.php';
 
-$fornecedores= listar('fornecedor');
+//DIRECIONANDO OS FORMULÁRIOS DE CADASTRO E EXCLUSÃO
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST['form-id'])) {
+        if($_POST['form-id'] == 'cadastrar_fornecedor') {
+            crudFornecedor(1, '');
+        } elseif ($_POST['form-id'] == 'excluir_fornecedor') {
+            crudFornecedor(3, $_POST['id']);
+        }
+    }
+}
+
+$_SESSION['tabela'] = 'fornecedor';
 
 //PUXANDO O HEADER, NAV E DEFININDO VARIÁVEIS 
-session_start();
 $tituloPagina = "FORNECEDORES";
 $tituloH1= "GESTÃO FORNECEDORES";
 include '../header.php';
@@ -13,51 +24,174 @@ include '../header.php';
 ?>
 
 <main class="main-content">
-    <div class="titulo">
-        <h2>CADASTRAMENTO</h2>
-    </div>
     <div class="top-section">
         <div class="actions-section">
-            <a href="fornecedor-cadastro.php" class="action-btn">
-                <span class="plus-icon">+ NOVO FORNECEDOR</span>
-            </a>     
+            <h2>CADASTRAMENTO</h2>
+            <button class="action-btn" onclick="abrePopup('popupCadastroFornecedor')"><span class="plus-icon">+</span>NOVO FORNECEDOR</button>
         </div>
     </div>
     
-    <div class = "titulo">
-        <h2>Fornecedores</h2>
+    <div class="search-section">
+        <h2>FORNECEDORES</h2>
+        <div class='search-section__barra'>
+            <i class='fi fi-rs-search'></i>
+            <input type="text" class="search-input" id="pesquisaInput" size="26";
+                   placeholder="Pesquisar ID, nome ou CNPJ" oninput="pesquisarDadoTabela('fornecedor')">
+        </div>
     </div>
 
     <div class='titleliv'>
-        <div class="tabela">
+        <div class="tabela" id="container-tabela">
             <div class="tisch">
-                <table>
-                    <tr>
-                        <th>Nome</th>
-                        <th>CNPJ</th>
-                        <th>Telefone</th>
-                        <th>E-mail</th>
-                        <th>Endereço</th>
-                        <th>Ação</th>
-                    </tr>
-                    <?php foreach ($fornecedores as $fornecedor): ?>
-                        <tr>
-                            <td><?= htmlspecialchars($fornecedor["forn_nome"]) ?></td>
-                            <td><?= htmlspecialchars($fornecedor["forn_cnpj"]) ?></td>
-                            <td><?= htmlspecialchars($fornecedor["forn_telefone"]) ?></td>
-                            <td><?= htmlspecialchars($fornecedor["forn_email"]) ?></td>
-                            <td><?= htmlspecialchars($fornecedor["forn_endereco"]) ?></td>
-                            <td>
-                                <i class='fas fa-trash-alt'
-                                    style="font-size: 20px; color: #a69c60; margin-right: 7px;"></i>
-                                <i class="fas fa-pencil-alt" style="font-size: 20px; color: #a69c60;"></i>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </table>
+                <?php include 'tabelas.php'; ?>
             </div>
         </div>
     </div>
 </main>
+
+<!--POPUP CADASTRAMENTO-->
+<dialog class="popup" id="popupCadastroFornecedor">
+<div class="popup-content">
+<div class="container">
+<h1>CADASTRAMENTO FORNECEDOR</h1>
+    <form method="POST">
+        <div class="form-row">
+            <div class="form-group">
+                <input type="hidden" name="form-id" value="cadastrar_fornecedor">
+                <label for="forn_nome">Nome: </label>
+                <input type="text" name="forn_nome" onkeypress="mascara(this,nomeMasc)" required>
+            </div>
+            <div class="form-group">
+                <label for="forn_cnpj">CNPJ: </label>
+                <input type="text" name="forn_cnpj" required 
+                    title="00.000.000/0000-00"
+                    maxlength="18"
+                    onkeypress="mascara(this,cnpjMasc)">
+            </div>
+        </div>
+
+        <div class="form-row">
+            <div class="form-group">
+                <label for="forn_telefone">Telefone: </label>
+                <input type="tel" name="forn_telefone" required
+                    title="(00) 00000-0000"
+                    maxlength="15"
+                    onkeypress="mascara(this,telefoneMasc)">
+            </div>
+
+            <div class="form-group">
+                <label for="forn_email">Email: </label>
+                <input type="email" name="forn_email" required>
+            </div>
+        </div>
+
+
+        <div class="form-row">
+            <div class="form-group">
+                <label for="forn_dataInscricao">Data Inscrição: </label>
+                <input type="date" name="forn_dataInscricao" required
+                    value="<?php echo date('Y-m-d'); ?>">
+            </div>
+
+            <div class="form-group">
+                <label for="forn_endereco">Endereço: </label>
+                <input type="text" name="forn_endereco" required>
+            </div>
+        </div>
+
+        <div class="button-group">
+            <button class="btn btn-save" type="submit">Cadastrar</button>
+            <button class="btn btn-cancel" onclick="fechaPopup('popupCadastroFornecedor')">Cancelar</button>
+        </div>
+    </form>
+</div>
+</div>
+</dialog>
+
+<!--POPUP EDIÇÃO-->
+<dialog class="popup" id="popupEdicaoFornecedor">
+<?php
+
+    if (isset($_GET['id'])) {
+        $idFornecedor = $_GET['id'];
+        $fornecedor = selecionarPorId('fornecedor', $idFornecedor, 'pk_forn');
+        
+        if (!$fornecedor) {
+            echo "<script>window.location.href='fornecedor-gestao.php';</script>";
+            exit();
+        }
+    }
+
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(isset($_POST['form-id'])) {
+            if($_POST['form-id'] === 'editar_fornecedor') {
+                crudFornecedor(2, $idFornecedor);
+            }
+        }
+    }
+
+    if ($fornecedor) :
+?>
+<div class="popup-content">
+<div class="container">
+<h1>EDIÇÃO FORNECEDOR</h1>
+    <form method="POST">
+        <div class="form-row">
+            <div class="form-group">
+                <input type="hidden" name="form-id" value="editar_fornecedor">
+                <input type="hidden" name="editar-id" value="<?= $idFornecedor ?? '' ?>">
+                <label for="forn_nome">Nome: </label>
+                <input type="text" name="forn_nome" onkeypress="mascara(this,nomeMasc)" required value="<?=$fornecedor['forn_nome']?>">
+            </div>
+            <div class="form-group">
+                <label for="forn_cnpj">CNPJ: </label>
+                <input type="text" name="forn_cnpj" required  
+                    title="00.000.000/0000-00"
+                    maxlength="18"
+                    onkeypress="mascara(this,cnpjMasc)"
+                    value="<?=$fornecedor['forn_cnpj']?>">
+            </div>
+        </div>
+
+        <div class="form-row">
+            <div class="form-group">
+                <label for="forn_telefone">Telefone: </label>
+                <input type="tel" name="forn_telefone" required
+
+                    title="(00) 00000-0000"
+                    maxlength="15"
+                    onkeypress="mascara(this,telefoneMasc)"
+                    value="<?=$fornecedor['forn_telefone']?>">
+            </div>
+
+            <div class="form-group">
+                <label for="forn_email">Email: </label>
+                <input type="email" name="forn_email" required value="<?=$fornecedor['forn_email']?>">
+            </div>
+        </div>
+
+
+        <div class="form-row">
+            <div class="form-group">
+                <label for="forn_dataInscricao">Data Inscrição: </label>
+                <input type="date" name="forn_dataInscricao" required
+                    value="<?=$fornecedor['forn_dataInscricao']?>">
+            </div>
+
+            <div class="form-group">
+                <label for="forn_endereco">Endereço: </label>
+                <input type="text" name="forn_endereco" required value="<?=$fornecedor['forn_endereco']?>">
+            </div>
+        </div>
+
+        <div class="button-group">
+            <button class="btn btn-save" type="submit">Alterar</button>
+            <button class="btn btn-cancel" type="button" onclick="location.href='fornecedor-gestao.php'">Cancelar</button>
+        </div>
+    </form>
+</div>
+</div>
+<?php endif; ?>
+</dialog>
 </body>
 </html>
